fb57d73e8b841bd9406a392adc7bf7e064a52f15
Mark Ross <krazkidd@gmail.com>
2023-05-15 14:31:15 -0700

    Update root example to use Pico Unicorn.
    Also, search for libraries using environment variables.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
-#include "pico_explorer.hpp"
-#include "drivers/st7789/st7789.hpp"
+#include <stdlib.h>
+#include <time.h>
+#include <list>
+
+#include "pico/stdlib.h"
+
 #include "libraries/pico_graphics/pico_graphics.hpp"
+#include "libraries/pico_unicorn/pico_unicorn.hpp"
 
-using namespace pimoroni;
+using pimoroni::PicoUnicorn;
+using pimoroni::PicoGraphics_PenRGB565;
+using pimoroni::Point;
 
-ST7789 st7789(PicoExplorer::WIDTH, PicoExplorer::HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
-PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
+PicoUnicorn pico_unicorn;
+PicoGraphics_PenRGB565 graphics(PicoUnicorn::WIDTH, PicoUnicorn::HEIGHT, nullptr);
 
 int main() {
+    stdio_init_all();
+
+    pico_unicorn.init();
+    pico_unicorn.clear();
+
     graphics.set_pen(255, 0, 0);
 
-    while(true) {
+    while (true) {
         graphics.pixel(Point(0, 0));
 
-        // now we've done our drawing let's update the screen
-        st7789.update(&graphics);
+        pico_unicorn.update(&graphics);
     }
 }
